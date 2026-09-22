56fb6a29dbff9cd56f1c409d015be37af70d2cb3
Sofia Isabelli <163188436+SofiaIsabelli@users.noreply.github.com>
2026-09-22 17:03:31 -0300

    organiza arquivos

---
+++ kornos/scripts/game_state.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var current_day: int = 1
+var level: int = 1
+var xp: int = 0
+var money: float = 100.0
+
+var unlocked_areas = {
+	"facade": true,
+	"main_store": true,
+	"shelves": true,
+	"storage": true,
+	"garage": false,
+	"produce": false
+}
